0ab35425c87786298b3739f74703ee48b5cb5a0f
AdrianD97 <adrianstefan376@gmail.com>
2020-06-19 11:29:58 +0300

    make all queries to run asynchronously

--- scripts/create_tables.js
+++ scripts/create_tables.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 
 const config = require('../config/config');
+const { tables } = require('../config/config');
 
-function create_table(conConfig, table, query) {
+function create_tables(conConfig, tables, queries) {
     const con = mysql.createConnection(conConfig);
 
     con.connect((err) => {
@@ -10,14 +11,21 @@ function create_table(conConfig, table, query) {
             throw err;
         }
 
-        con.query(query, (err, result) => {
+        con.query(queries[0], (err, result) => {
             if (err) {
                 throw err;
             }
 
-            console.log('Table ' + table + ' created successfully.');
+            console.log('Table ' + tables[0] + ' created successfully.');
 
-            con.end();
+            con.query(queries[1], (err, result) => {
+                if (err) {
+                    throw err;
+                }
+
+                console.log('Table ' + tables[1] + ' created successfully.');
+                con.end();
+            });
         });
     });
 }
@@ -26,9 +34,9 @@ try {
     const conConfig = config['database'];
     conConfig['database'] = config['database_name'];
 
-    let table = config['tables'][0];
+    const tables = config['tables'];
 
-    let query = 'CREATE TABLE ' + table
+    const query1 = 'CREATE TABLE ' + tables[0]
                 + '(id INT AUTO_INCREMENT PRIMARY KEY, '
                 + 'name VARCHAR(50) NOT NULL, '
                 + 'birthday DATE, '
@@ -36,22 +44,17 @@ try {
                 + 'promoted BOOLEAN DEFAULT false, '
                 + 'diff INT NOT NULL, '
                 + 'CONSTRAINT check_diff CHECK (diff >= 1 AND diff <= 8))';
+    
+    const query2 = 'CREATE TABLE ' + tables[1]
+                + '(grade_id INT AUTO_INCREMENT PRIMARY KEY, '
+                + 'grade INT NOT NULL, '
+                + 'discipline VARCHAR(10) NOT NULL, '
+                + 'grade_date DATE, '
+                + 'student_id INT, '
+                + 'CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES persons(id), '
+                + 'CONSTRAINT check_grade CHECK (grade >= 1 AND grade <= 10))';
 
-    create_table(conConfig, table, query);
-
-
-    table = config['tables'][1];
-
-    query = 'CREATE TABLE ' + table
-            + '(grade_id INT AUTO_INCREMENT PRIMARY KEY, '
-            + 'grade INT NOT NULL, '
-            + 'discipline VARCHAR(10) NOT NULL, '
-            + 'grade_date DATE, '
-            + 'student_id INT, '
-            + 'CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES persons(id), '
-            + 'CONSTRAINT check_grade CHECK (grade >= 1 AND grade <= 10))';
-
-    create_table(conConfig, table, query);
+    create_tables(conConfig, tables, [ query1, query2 ]);
 } catch (e) {
     console.log(e);
 }
